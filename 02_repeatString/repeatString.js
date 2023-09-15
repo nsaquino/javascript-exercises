d700400018938eed6608684d80e996e7185ce663
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-15 10:36:15 -0300

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let res = "";
+    while (num--) {
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
