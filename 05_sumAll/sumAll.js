26db2802a6484c99cfd3e60cfbcba5477ebddcab
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-19 15:08:20 -0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    if (typeof(a) !== 'number' || typeof(b) !== 'number') return 'ERROR';
+    if (isNaN(a)|| isNaN(b)) return 'ERROR';
 
+    let sum = 0;
+    const from = Math.min(a,b);
+    const to = Math.max(a,b);
+    for (let i = from; i <= to; i++) {
+        sum += i;        
+    }
+    return sum;
 };
 
 // Do not edit below this line
