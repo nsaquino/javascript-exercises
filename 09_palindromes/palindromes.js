007dd4bc9a1d05193345c8c3561442283a7f6373
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-24 16:12:36 -0300

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let onlyAlpha = str.toLowerCase().split(/\W+/g);
+    let reversed = onlyAlpha;
 
+    onlyAlpha = onlyAlpha.join("").split("").join(""); //Join into a string, then reverse letters
+    reversed = reversed.join("").split("").reverse().join("");
+
+    console.log(onlyAlpha, reversed);
+    return onlyAlpha === reversed;
 };
 
 // Do not edit below this line
