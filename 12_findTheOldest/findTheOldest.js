8dac8a224aef1d91c028f8fcdd1453d47a9d0685
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-24 17:06:03 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function (obj) {
+    const sorted = obj.sort((lastOne, nextOne) => {
+        let lastOneComparisonYear;
+        let nextOneComparisonYear;
+        
+        if (!lastOne.yearOfDeath) {
+            lastOneComparisonYear = (new Date()).getFullYear();
+        } else {
+            lastOneComparisonYear = lastOne.yearOfDeath;
+        }
 
+        if (!nextOne.yearOfDeath) {
+            nextOneComparisonYear = (new Date()).getFullYear();
+        } else {
+            nextOneComparisonYear = nextOne.yearOfDeath;
+        }
+
+        /* console.log(lastOne);
+        console.log(nextOne); */
+
+        ageLast = lastOneComparisonYear - lastOne.yearOfBirth;
+        ageNext = nextOneComparisonYear - nextOne.yearOfBirth;
+
+        return (ageLast > ageNext) ? -1 : 1;
+    });
+
+    return sorted[0];
 };
 
 // Do not edit below this line
